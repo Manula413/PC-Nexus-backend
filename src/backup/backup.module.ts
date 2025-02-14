85fd91891d289b6250159d1a0f6d314c5b91d1de
Manula Pasan <89297752+Manula413@users.noreply.github.com>
2025-02-14 14:30:15 +0530

    added cron job to backup DB

--- src/backup/backup.module.ts
+++ src/backup/backup.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { BackupService } from './backup.service';
+import { PrismaService } from '../prisma.service';
 
 @Module({
-  providers: [BackupService]
+  imports: [ScheduleModule.forRoot()],
+  providers: [BackupService, PrismaService],
 })
 export class BackupModule {}
--- src/backup/backup.service.ts
+++ src/backup/backup.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { exec } from 'child_process';
+import { join } from 'path';
+import { existsSync, mkdirSync } from 'fs';  // For checking and creating the backup directory
 
 @Injectable()
-export class BackupService {}
+export class BackupService {
+
+    constructor() { }
+
+    @Cron(CronExpression.EVERY_MINUTE)
+    handleCron() {
+        console.log('Starting SQLite database backup...');
+        this.backupDatabase();
+    }
+
+    backupDatabase() {
+        const dbPath = join(__dirname, '../../prisma', 'shop.db');
+        
+        // Ensure the backup directory exists
+        const backupDir = join(__dirname, '../../backup');
+        if (!existsSync(backupDir)) {
+            mkdirSync(backupDir, { recursive: true });  // Create the backup folder if it doesn't exist
+        }
+
+        const backupPath = join(
+            backupDir,
+            `shop_backup_${new Date().toISOString().replace(/:/g, '-').replace(/\./g, '-')}.sql`
+        );
+
+        const command = `sqlite3 ${dbPath} .dump > ${backupPath}`;
+
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error during backup: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`Database backup completed! Backup saved at ${backupPath}`);
+        });
+    }
+}
